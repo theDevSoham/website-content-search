389adaff558b24c4dd14d798811e95f892d3c798
Soham Das <publicsoham.24@gmail.com>
2025-09-04 11:55:27 +0530

    fix: fixed dockerfile

--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-uv run uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 --reload-dir app
+# uv run uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 --reload-dir app
 # uv run gunicorn -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:8000 --workers 1 --reload --reload-dir app
+docker build -t website_content_search2 . && docker run -p 8000:8000 --env-file .env website_content_search2
